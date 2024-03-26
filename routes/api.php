fef6fc65f0b3794d1488cd8b30f85d899d6c47b1
kholoud001 <kholoud.sanak@gmail.com>
2024-03-26 11:07:14 +0000

    api routing

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::apiResource('users', UserController::class);
+Route::namespace('Api')->group(function (){
+
+    Route::prefix('auth')->group(function (){
+        Route::post('login', [AuthController::class, 'login']);
+        Route::post('register', [AuthController::class, 'register']);
+    });
+    Route::group([
+        'middleware'=> 'auth:api'
+    ],function (){
+        Route::get('helloworld', [AuthController::class, 'index']);
+        Route::post('logout',[AuthController::class,'logout']);
+    });
 });

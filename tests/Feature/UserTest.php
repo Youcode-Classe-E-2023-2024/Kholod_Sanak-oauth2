7d97ee9b814674ddb76afca19d3a23225322638c
kholoud001 <kholoud.sanak@gmail.com>
2024-03-27 12:09:02 +0000

    Update UserTest.php

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,46 +1,39 @@
 <?php
-//
-//namespace Tests\Feature;
-//
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//use Illuminate\Foundation\Testing\WithFaker;
-//use Tests\TestCase;
-//use App\Models\User; // Import the User model
-//
-//class UserTest extends TestCase
-//{
-//    use RefreshDatabase; // Use this trait to run tests with a clean database
-//
-//    /**
-//     * Test adding a user when authenticated.
-//     *
-//     * @return void
-//     */
-//    public function test_add_user_when_authenticated()
-//    {
-//        $this->artisan('migrate');
-//
-//        // Create a user for authentication
-//        $user = User::factory()->create();
-//
-//        // Authenticate the user
-//        $this->actingAs($user, 'api');
-//
-//        // Data for the new user
-//        $userData = [
-//            'name' => 'John Doe',
-//            'email' => 'john@example.com',
-//            'password' => 'password123',
-//            'role_id' => 2,
-//        ];
-//
-//        // Make a POST request to add the user
-//        $response = $this->postJson('/api/users', $userData); // Use postJson() for JSON requests
-//
-//        // Assert that the response status is 201 (Created) since the user is authenticated
-//        $response->assertStatus(201);
-//
-//        // Assert that the response contains the user data
-//        $response->assertJsonFragment($userData);
-//    }
-//}
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+//    use RefreshDatabase;
+
+
+    public function test_show_user_when_authenticated()
+    {
+        // Create an admin user
+        $adminRole = Role::where('name', 'admin')->first();
+        $adminUser = User::factory()->create(['role_id' => $adminRole->id]);
+
+        // Authenticate the admin user
+        $this->actingAs($adminUser, 'api');
+
+        // Create another user for testing
+        $defaultRole = Role::where('name', 'user')->first();
+        $userToShow = User::factory()->create(['role_id' => $defaultRole->id]);
+
+        // Make a GET request to show the user
+        $response = $this->getJson("/api/user/{$userToShow->id}");
+
+        // Assert that the response status is 200 (OK) since the user is authenticated
+        $response->assertStatus(200);
+
+        // Assert that the response contains the user data
+        $response->assertJsonFragment($userToShow->toArray());
+    }
+
+}
